d7a4c78bd0d6a34edf34987a178ff0b631a44cf0
yaralahruthik <32637966+yaralahruthik@users.noreply.github.com>
2020-11-22 14:50:38 +0530

    firebase write update for tasks

--- src/store/store-tasks.js
+++ src/store/store-tasks.js
@@ -46,19 +46,19 @@ const mutations = {
 }
 
 const actions = {
-  updateTask({ commit }, payload) {
-    commit('updateTask', payload)
+  updateTask({ dispatch }, payload) {
+    dispatch('fbUpdateTask', payload)
   },
-  deleteTask({ commit }, id) {
-    commit('deleteTask', id)
+  deleteTask({ dispatch }, id) {
+    dispatch('fbDeleteTask', id)
   },
-  addTask({ commit }, task) {
+  addTask({ dispatch }, task) {
     let taskId = uid()
     let payload = {
       id: taskId,
       task: task
     }
-    commit('addTask', payload)
+    dispatch('fbAddTask', payload)
   },
   setSearch({ commit }, value) {
     commit('setSearch', value)
@@ -95,6 +95,21 @@ const actions = {
       let taskId = snapshot.key
       commit("deleteTask", taskId)
     })
+  },
+  fbAddTask({}, payload) {
+    let userId = firebaseAuth.currentUser.uid;
+    let taskRef = firebaseDb.ref('tasks/' + userId + '/' + payload.id)
+    taskRef.set(payload.task)
+  },
+  fbUpdateTask({}, payload) {
+    let userId = firebaseAuth.currentUser.uid;
+    let taskRef = firebaseDb.ref('tasks/' + userId + '/' + payload.id)
+    taskRef.update(payload.updates)
+  },
+  fbDeleteTask({}, taskId) {
+    let userId = firebaseAuth.currentUser.uid;
+    let taskRef = firebaseDb.ref('tasks/' + userId + '/' + taskId)
+    taskRef.remove()
   }
 }
 
