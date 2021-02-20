23c636123a6e82d88df2785c7a95baf7a4bb114c
yaralahruthik <32637966+yaralahruthik@users.noreply.github.com>
2021-02-20 14:27:04 +0530

    Github Pages Deploy

--- src/functions/function-show-error-message.js
+++
@@ -1,9 +0,0 @@
-import { Dialog, Loading } from 'quasar'
-
-export function showErrorMessage(errorMessage) {
-  Loading.hide()
-  Dialog.create({
-    title: 'Error',
-    message: errorMessage
-  })
-}
